420ea274eae710c24d8a09808a4236c6928f5801
katesmiles <kate.smiles@readingpartners.org>
2016-03-17 12:44:39 -0600

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -10,33 +10,35 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-      inv <- NULL
-      set <- function(y) {
-            x <<- y
-            inv <<- NULL
+makeCacheMatrix <- function(z = matrix()) {
+      invrs <- NULL
+      set <- function(y) { 
+      		## use the new <<- operator to assign a value to an object stored in an environment other than the 	current env. 
+            z <<- y
+            invrs <<- NULL
       }
-      get <- function() x
-      setInverse <- function(inverse) inv<<- inverse
-      getInverse <- function() inv
+      get <- function() z
+      setinv <- function(inverse) invrs<<- inverse
+      getinv <- function() invrs
       list(set = set,
            get = get,
-           setInverse = setInverse,
-           getInverse = getInverse)
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## This function computes the inverse of the special "matrix" created by makeCacheMatrix.
+## This function computes the inverse of the special "matrix" created by makeCacheMatrix. It returns the inverse of the original matrix in makeCacheMatrix (above)
 
-cacheSolve <- function(x, ...) {
-      ## Returns a matrix that is the inverse of 'x'
-      inv <- x$getInverse()
-      if (!is.null(inv)) {
-            message("retrieving cached data")
-            return(inv)
+cacheSolve <- function(z, ...) {
+      ## Returns a matrix that is the inverse of 'z'
+      invrs <- z$getinv()
+      if (!is.null(invrs)) { ## R returns from the cache and does not compute the inverse if the inverse has already been computed. So, the first time I run cacheSolve the message below does not return; the second time I call the function, it does
+            message("retrieving cached data - inverse previously computed")
+            return(invrs)
       }
-      mat <- x$get()
-      inv <- solve(mat, ...)
-      x$setInverse(inv)
-      inv
+      ## if inverse has not yet been calculated, the function calculates the inverse.
+      mtx <- z$get()
+      invrs <- solve(mtx, ...)
+      z$setinv(invrs)
+      invrs
 }
