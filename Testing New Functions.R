7a02ff30a7613cc770fe53d3bf589ce4c823b2a2
katesmiles <kate.smiles@readingpartners.org>
2016-03-17 12:45:30 -0600

    Added files via upload

--- Testing New Functions.R
+++ Testing New Functions.R
@@ -12,17 +12,17 @@ test_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
 test_matrix$get()
 
 ##Creating the inverse of the test matrix.
-test_matrix$getInverse()
+test_matrix$getinv()
 cacheSolve(test_matrix)
 
 ##Runing cachSolve function now that inverse has been cached.
 cacheSolve(test_matrix)
-test_matrix$getInverse()
+test_matrix$getinv()
 
 ##Setting new values of test matrix.
 test_matrix$set(matrix(c(4, 4, 2, 1), 2, 2))
 test_matrix$get()
-test_matrix$getInverse()
+test_matrix$getinv()
 cacheSolve(test_matrix)
 cacheSolve(test_matrix)
-test_matrix$getInverse()
+test_matrix$getinv()
